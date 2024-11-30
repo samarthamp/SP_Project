e3fdc4303433b920d29c73c4d78c29fc76507400
Kiruba-Selvakumar <kiruba.selvakumar@research.iiit.ac.in>
2024-12-01 05:09:02 +0530

    hello

--- Qn3_LoudnessSegmentation/Task b/Task_b.m
+++ Qn3_LoudnessSegmentation/Task b/Task_b.m
@@ -7,21 +7,37 @@ audioFiles = {'./audios/1.wav'};
 
 for i = 1:length(audioFiles)
 
+    [audioData, fs] = audioread(audioFiles{i});
+
     % Convert to mono if stereo
     if size(audioData, 2) > 1
         audioData = mean(audioData, 2);
     end
 
-    [audioData, fs] = audioread(audioFiles{i});
     N = length(audioData);
     mag_spectra = fftshift(abs(fft(audioData)));
 
+    pRMS = rms(audioData).^2;
+    powbp = bandpower(audioData);
+    [wd,lo,hi,power] = obw(audioData,fs);
+    powtot = power/0.99;
+
+    [p, f, t] = pspectrum(audioData, fs, 'spectrogram');
+    figure
+        waterfall(f,t,p');
+        xlabel('f');
+        xlim([0 0.5*1e4]);
+        ylabel('t');
+        zlabel('p');
+
     figure;
-        subplot(2, 1, 1)
+        subplot(3, 1, 1)
             plot(linspace(0, N/fs, N), audioData);
-        subplot(2, 1, 2)
-            plot(-(N-1)/2:(N/fs):(N-1)/2, mag_spectra);
+        subplot(3, 1, 2)
+            % plot(linspace(0, N/fs, N), (audioData));
+        subplot(3, 1, 3)
+            plot(-(N-1)/2:1:(N-1)/2, mag_spectra);
+            
+        
     
 end
-disp(['Mean = ', num2str(mean(sortedIntervals(:, 3)))]);
-disp(['Standard Deviation = ', num2str(std(sortedIntervals(:, 3)))]);
