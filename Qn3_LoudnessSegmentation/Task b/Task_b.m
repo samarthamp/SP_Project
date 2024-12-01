fee45fbe3fd09d5c476f5527321e389334daa9bd
Kiruba-Selvakumar <kiruba.selvakumar@research.iiit.ac.in>
2024-12-01 18:11:55 +0530

    qn3b partially done

--- Qn3_LoudnessSegmentation/Task b/Task_b.m
+++ Qn3_LoudnessSegmentation/Task b/Task_b.m
@@ -3,41 +3,42 @@ clearvars, clc, close all;
 % List of audio and text file names
 % audioFiles = {'./audios/1.wav', './audios/2.wav', './audios/3.wav', './audios/4.wav',  './audios/5.wav',  './audios/6.wav',  './audios/7.mp3',  './audios/8.mp3', './audios/9.mp3'};
 % textFiles = {'./text/1.txt', './text/2.txt', './text/3.txt',  './text/4.txt',  './text/5.txt',  './text/6.txt',  './text/7.txt',  './text/8.txt', './text/9.txt'};
-audioFiles = {'./audios/1.wav'};
+audioFiles = {'./audios/2.wav'};
 
 for i = 1:length(audioFiles)
 
     [audioData, fs] = audioread(audioFiles{i});
-
-    % Convert to mono if stereo
-    if size(audioData, 2) > 1
-        audioData = mean(audioData, 2);
-    end
+    audioData = audioData';
 
     N = length(audioData);
     mag_spectra = fftshift(abs(fft(audioData)));
 
-    pRMS = rms(audioData).^2;
-    powbp = bandpower(audioData);
-    [wd,lo,hi,power] = obw(audioData,fs);
-    powtot = power/0.99;
+    % Defining segment/window
+    segment = ones(1, ceil(N/1e5));
+    seglen = length(segment);
 
-    [p, f, t] = pspectrum(audioData, fs, 'spectrogram');
-    figure
-        waterfall(f,t,p');
-        xlabel('f');
-        xlim([0 0.5*1e4]);
-        ylabel('t');
-        zlabel('p');
+    seg_energy = zeros(1, N);
+
+    % Append zeros of length seglen-1 to allow overflow of window (slide all the way to the end)
+    sq_signal = [audioData.^2 zeros(1, seglen-1)];
+    
+    % Peak amplitude in a segment
+    seg_peak = zeros(1, N);
+
+    for ix = 1:N
+        seg_energy(ix) = sum(sq_signal(ix:ix+seglen-1));
+        seg_peak(ix) = max(sq_signal(ix:ix+seglen-1));
+    end
+
+    avg_seg_energy = mean(seg_energy);
+    normalized_seg_energy =  seg_energy./max(seg_energy);
 
     figure;
         subplot(3, 1, 1)
-            plot(linspace(0, N/fs, N), audioData);
+            plot(linspace(0, N/fs, N), seg_energy);
         subplot(3, 1, 2)
-            % plot(linspace(0, N/fs, N), (audioData));
+            plot(linspace(0, N/fs, N), avg_seg_energy);
         subplot(3, 1, 3)
-            plot(-(N-1)/2:1:(N-1)/2, mag_spectra);
-            
-        
-    
+            plot(linspace(0, N/fs, N), seg_peak);
 end
+
